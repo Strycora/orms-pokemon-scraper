57c6394bcd279c04c2edacac3fbace28ab299ffe
Vikram Chatterjee <strycora@gmail.com>
2020-12-10 12:51:12 -0700

    commit tree cda4dcf5c2169117823f77983df8c98c4e98115b

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,35 @@
+require 'pry'
 class Pokemon
+    attr_reader :id
+    attr_accessor :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon (name, type) 
+        VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon 
+        WHERE ? is #{id}
+        SQL
+        data = db.execute(sql, id).flatten
+        
+        pokemon = Pokemon.new(id: data[0], name: data[1], type: data[2], db: db)
+    end
+
+
 end
